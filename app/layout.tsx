622b47cb378bdd942e15a08e8cbbb44f5b6ad743
Akshaj Dev <akshajdevkv@gmail.com>
2025-08-02 14:18:58 +0530

    setup localFont

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,41 @@
 import Navbar from "./components/Navbar"
+import localFont from "next/font/local"
 import "./globals.css"
+
+
+const workSans = localFont({
+  src: [
+    {
+      path: "./fonts/WorkSans-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./fonts/WorkSans-Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "./fonts/WorkSans-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "./fonts/WorkSans-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "./fonts/WorkSans-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "./fonts/WorkSans-ExtraLight.ttf",
+      weight: "200",
+    },
+    {
+      path: "./fonts/WorkSans-Thin.ttf",
+      weight: "100",
+    },
+    ]
+})
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
