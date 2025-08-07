8a1ca50226dc4fb5c777a6f28508f0c5e7d05e0b
Akshaj Dev <akshajdevkv@gmail.com>
2025-08-07 17:06:26 +0530

    implement increment view feature for startup pages

--- components/View.tsx
+++ components/View.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import Ping from './Ping'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
 import { client } from '@/sanity/lib/client'
+import { writeClient } from '@/sanity/lib/write-client'
+import { after } from "next/server" 
 
 const View = async ({id}:{id:string}) => {
    const  {views:totalViews}= await client.fetch(STARTUP_VIEWS_QUERY,{id})
-
+   
+   // Increment view count AFTER response is sent
+   after(async () => {
+     await writeClient.patch(id).set({views:totalViews+1}).commit()
+   })
+   
   return <> 
     
         <div className="view-container">
